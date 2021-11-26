89dcc429cbc50fd71e9656a19e98d4ce0d3da52f
yushaojian <yushaojian13@163.com>
2021-11-26 17:21:09 +0800

    [feat] https://github.com/jaredrummler/ColorPicker

--- app/src/main/java/com/art/cp/demo/MainActivity.kt
+++ app/src/main/java/com/art/cp/demo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.art.cp.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.art.colorpicker.ColorPickerView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ColorPickerView.OnColorChangedListener {
+
+    private lateinit var colorPickerView: ColorPickerView
+    private lateinit var colorTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        colorPickerView = findViewById(R.id.colorPickerView)
+        colorTV = findViewById(R.id.colorTV)
+
+        onColorChanged(colorPickerView.color)
+        colorPickerView.setOnColorChangedListener(this)
+    }
+
+    override fun onColorChanged(newColor: Int) {
+        colorTV.setBackgroundColor(newColor)
+        colorTV.text = String.format(Locale.ENGLISH, "%X", newColor)
     }
 }
