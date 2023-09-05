699c560c122f7c90f86e5f89183b3b77f8824d6e
yushaojian <yushaojian13@163.com>
2023-09-05 15:38:54 +0800

    AlphaPatternDrawable

--- ColorPicker/src/main/java/com/art/color/picker/AlphaPattern.kt
+++ ColorPicker/src/main/java/com/art/color/picker/AlphaPattern.kt
@@ -12,26 +12,28 @@ const val GRAY_COLOR: Int = -0x343435 // 0xFFCBCBCB
 const val COLOR_LUMP_SIZE = 10
 
 fun createAlphaPatternShader(): Shader {
-    return createAlphaPatternShader((COLOR_LUMP_SIZE.dp + 0.5f).toInt())
+    return createAlphaPatternShader((COLOR_LUMP_SIZE.dp + 0.5f).toInt())!!
 }
 
-fun createAlphaPatternShader(size: Int): Shader {
-    val alphaBitmap = createAlphaPatternBitmap(size)
+fun createAlphaPatternShader(size: Int): Shader? {
+    val alphaBitmap = createAlphaPatternBitmap(size) ?: return null
     return BitmapShader(alphaBitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT)
 }
 
-fun createAlphaPatternBitmap(size: Int): Bitmap {
+fun createAlphaPatternBitmap(size: Int): Bitmap? {
+    if (size <= 0) return null
+
     val alphaPatternPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(bitmap)
-    val middle = size / 2f
+    val half = size / 2f
     for (i in 0..1)
         for (j in 0..1) {
             if ((i + j) % 2 == 0)
                 alphaPatternPaint.color = GRAY_COLOR
             else
                 alphaPatternPaint.color = WHITE_COLOR
-            canvas.drawRect((i * middle), (j * middle), ((i + 1) * middle), ((j + 1) * middle), alphaPatternPaint)
+            canvas.drawRect((i * half), (j * half), ((i + 1) * half), ((j + 1) * half), alphaPatternPaint)
         }
     return bitmap
 }
---
+++ ColorPicker/src/main/java/com/art/color/picker/AlphaPatternDrawable.kt
@@ -0,0 +1,35 @@
+package com.art.color.picker
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+
+/**
+ * This drawable will draw a simple white and gray chessboard pattern.
+ * It's the pattern you will often see as a background behind a partly transparent image in many applications.
+ */
+class AlphaPatternDrawable(rectangleSize: Int) : Drawable() {
+
+    private val alphaPatternPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        shader = createAlphaPatternShader(rectangleSize * 2)
+    }
+
+    override fun draw(canvas: Canvas) {
+        val bounds = RectF(bounds)
+        canvas.translate(bounds.left, bounds.top)
+        canvas.drawRect(0f, 0f, bounds.width(), bounds.height(), alphaPatternPaint)
+        canvas.translate(-bounds.left, -bounds.top)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        throw UnsupportedOperationException("Alpha is not supported by this drawable.")
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        throw UnsupportedOperationException("ColorFilter is not supported by this drawable.")
+    }
+
+    @Deprecated("Deprecated in Java", ReplaceWith("PixelFormat.OPAQUE", "android.graphics.PixelFormat"))
+    override fun getOpacity(): Int {
+        return PixelFormat.OPAQUE
+    }
+}
